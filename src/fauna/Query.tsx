870dd8190412270773ccff5d683671e36015cad3
Lucas Johnson <contact@lucasjohnson.co.nz>
2022-01-12 23:13:42 +0100

    refactored query file into one file

--- src/fauna/Query.tsx
+++ src/fauna/Query.tsx
@@ -1,4 +1,22 @@
-export class graphQL {
+import faunadb from 'faunadb';
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+  secret: process.env.FAUNA_CLIENT_SECRET,
+});
+
+export class Query {
+  public static createItem = (data: any): void => {
+    client
+      .query(
+        q.Create(q.Collection('Item'), {
+          data: data,
+        })
+      )
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
+  };
+
   public static getItems = async (): Promise<any> => {
     return await fetch('https://graphql.fauna.com/graphql', {
       method: 'POST',
